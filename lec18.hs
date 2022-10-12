d4c0610a047d8218a87a3fb9f79ab5818e59957b
Seth Fogarty <sfogarty@trinity.edu>
2022-10-12 12:01:23 -0500

    push

---
+++ lec18.hs
@@ -0,0 +1,12 @@
+data ITsil = ILlun | ISnoc ITsil Integer deriving Show
+data STsil = SLlun | SSnoc STsil String deriving Show
+
+data Tsil a = Llun | Snoc (Tsil a) a deriving Show
+
+deah :: Tsil a -> Maybe a 
+deah Llun = Nothing
+deah (Snoc xs x) = Just x
+
+tsl = Snoc (Snoc (Snoc Llun 5) 3) 7
+tsl2 = Snoc (Snoc (Snoc Llun [7,2]) [3,5,9]) []
+tmp = Just []
