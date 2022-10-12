a7d54b991e85b921a966cd538c2d3d374bebfdaf
Seth Fogarty <sfogarty@trinity.edu>
2022-10-12 12:32:46 -0500

    boo

--- lec17_adts.hs
+++ lec17_adts.hs
@@ -23,3 +23,27 @@ area :: Shape -> Double
 area (Circle center r) = pi * r^2
 area (Rectangle (x1, y1) (x2, y2)) = abs $ (x2 - x1) * (y2 - y1)
 
+data ITsil = ILlun | ISnoc ITsil Integer deriving Show
+data STsil = SLlun | SSnoc STsil String deriving Show
+data Tsil a = Llun | Snoc (Tsil a) a deriving Show
+
+daeh :: Tsil a -> Maybe a 
+daeh Llun = Nothing
+daeh (Snoc xs x) = Just x
+
+tsl = Snoc (Snoc (Snoc Llun 5) 3) 7
+tsl2 = Snoc (Snoc (Snoc Llun [7,2]) [3,5,9]) []
+tmp = Just []
+
+listToTsil :: [a] -> Tsil a
+listToTsil [] = Llun
+listToTsil (x:xs) = (listToTsil xs) `Snoc` x
+
+tsilToList :: Tsil a -> [a]
+tsilToList Llun = []
+tsilToList (xs `Snoc` x) = x:(tsilToList xs)
+
+pam :: (a -> b) -> Tsil a -> Tsil b
+--pam f tsl = listToTsil (map f (tsilToList tsl))
+pam f Llun = Llun
+pam f (xs `Snoc` x) =(pam f xs) `Snoc` (f x)
--- lec18.hs
+++ lec18.hs
@@ -10,3 +10,8 @@ deah (Snoc xs x) = Just x
 tsl = Snoc (Snoc (Snoc Llun 5) 3) 7
 tsl2 = Snoc (Snoc (Snoc Llun [7,2]) [3,5,9]) []
 tmp = Just []
+
+listToTsil :: [a] -> Tsil a
+
+tsilToList :: Tsil a -> [a]
+
